58abc0c3639d0e42e7606feca35417b537749f3a
Tom Yeh <doubleshow@gmail.com>
2022-11-08 14:09:51 +0000

    1.0

---
+++ engine.cpp
@@ -0,0 +1,299 @@
+#include <iostream>     // cout, cin, streambuf, hex, endl, sgetc, sbumpc
+#include <iomanip>      // setw, setfill
+#include <fstream>      // fstream
+
+// These inclusions required to set terminal mode.
+#include <termios.h>    // struct termios, tcgetattr(), tcsetattr()
+#include <stdio.h>      // perror(), stderr, stdin, fileno()
+
+using namespace std;
+
+#ifdef OS_WIN
+#include <conio.h>
+#elif defined(OS_LINUX) || defined(OS_MAC)
+#include <unistd.h>
+#include <sys/socket.h>
+#endif
+bool stdinHasData()
+{
+#   if defined(OS_WIN)
+    // this works by harnessing Windows' black magic:
+    return _kbhit();
+// #   elif defined(OS_LINUX) || defined(OS_MAC) 
+#   else
+
+    struct termios t;
+    struct termios t_saved;
+
+    // Set terminal to single character mode.
+    tcgetattr(fileno(stdin), &t);
+    t_saved = t;
+    t.c_lflag &= (~ICANON & ~ECHO);
+    t.c_cc[VTIME] = 0;
+    t.c_cc[VMIN] = 1;
+    if (tcsetattr(fileno(stdin), TCSANOW, &t) < 0) {
+        perror("Unable to set terminal to single character mode");
+        return -1;
+    }
+
+
+    // using a timeout of 0 so we aren't waiting:
+    struct timespec timeout{ 0l, 0l };
+
+    // create a file descriptor set
+    fd_set fds{};
+
+    // initialize the fd_set to 0
+    FD_ZERO(&fds);
+    // set the fd_set to target file descriptor 0 (STDIN)
+    FD_SET(0, &fds);
+
+    // pselect the number of file descriptors that are ready, since
+    //  we're only passing in 1 file descriptor, it will return either
+    //  a 0 if STDIN isn't ready, or a 1 if it is.
+
+    
+
+    bool s = pselect(0 + 1, &fds, nullptr, nullptr, &timeout, nullptr) == 1;
+
+// Restore terminal mode.
+    if (tcsetattr(fileno(stdin), TCSANOW, &t_saved) < 0) {
+        perror("Unable to restore terminal mode");
+        return -1;
+    }
+    return s;
+// #   else
+//     // throw a compiler error
+//     static_assert(false, "Failed to detect a supported operating system!");
+#   endif
+}
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include <thread>
+
+
+#include <vector>
+#include "engine.h"
+
+void Canvas::createCanvas(int w, int h)
+{
+    _width = w;
+    _height = h;
+    clear();
+}
+
+void Canvas::stroke(const string symbol)
+{
+    _strokeSymbol = symbol;
+}
+
+void Canvas:: clear()
+{
+    for (int i = 0; i < _height; i++)
+    {
+        for (int j = 0; j < _width; j++)
+        {
+            buffer[i][j][0] = 0;
+            buffer[i][j][1] = 0;
+            buffer[i][j][2] = 0;
+            buffer[i][j][3] = 0;
+        }                
+    }
+}
+
+void Canvas::point(int x, int y)
+{
+    if (x >= 0 && x <  _width && y < _height && y >= 0)
+    {                            
+        point(x, y, _strokeSymbol);
+    }
+}
+
+void Canvas::point(int x_, int y_, string symbol)
+{
+    int x = _xo + x_;
+    int y = _yo + y_;
+
+    if (x >= 0 && x <  _width && y < _height && y >= 0)
+    {
+        for (int i = 0; i < symbol.size(); i++)
+        {
+            buffer[y][x][i] = symbol[i];
+        }             
+    }
+}
+
+void Canvas:: text(string word, int x_, int y_)
+{
+    int x = _xo + x_;
+    int y = _yo + y_;
+
+    int j = 0;
+    for (int i = 0; i < word.size(); i = i + 2)
+    {
+        j = x + (i / 2);
+        buffer[y][j][0] = word[i];
+        buffer[y][j][1] = word[i+1];   
+        buffer[y][j][2] = 0;
+    }
+    if (word.size() % 2 == 0)
+    {
+        buffer[y][j][2] = 0;
+    }
+    else 
+    {
+        buffer[y][j][0] = word[word.size()-1];
+        buffer[y][j][1] = ' ';
+        buffer[y][j][2] = 0;
+    }
+}
+
+void Canvas:: vertical_line(int x, int y1, int y2)
+{
+    if (x < _width && x >= 0)
+    {
+        for (int y = y1; y < y2; y++)
+        {
+            point(x, y);
+        }
+    }
+}
+
+void  Canvas::horizontal_line(int y, int x1, int x2)
+{
+    if (y < _width && y >= 0)
+    {
+        for (int x = x1; x < x2; x++)
+        {
+            point(x, y);
+        }
+    }
+}        
+
+void Canvas::render()
+{
+    cout << "p3 Engine " << ENGINE_VERSION << endl ;
+    cout << "   ";    
+    for (int i = 0; i < _width; i++)
+    {   
+        cout << setw(2) << i % 10;
+    }    
+    cout << endl;
+    cout << "  ┌";
+    for (int i = 0; i < _width; i++)
+    {   
+        cout << "──";
+    }
+    cout << "┐";
+    cout << endl;
+    for (int i = 0; i < _height; i++)
+    {
+        cout << setw(2) << i << "│";
+        for (int j = 0; j < _width; j++)
+        {                    
+            if (buffer[i][j][0])
+            {
+                wcout << buffer[i][j];
+            } 
+            else
+            {
+                cout << "  ";
+            }
+        }
+        cout << "│";
+        cout << endl;                
+    }
+    cout << "  └";
+    for (int i = 0; i < _width; i++)
+    {   
+        cout << "──";
+    }
+    cout << "┘";
+    cout << endl;
+}
+
+void Engine::quit()
+{
+    _quit = true;
+}
+
+void Engine::pause()
+{
+    cout << "Press [SPACE] to continue." << endl;
+    _pausedUntilSpaceIsPressed = true;
+}
+
+void Engine::play()
+{
+    setup();
+    int keyCode{ 0 };
+    while (!_quit)
+    {        
+        if (_pausedUntilSpaceIsPressed)            
+        {
+            if (stdinHasData())
+            { 
+                keyCode = std::cin.get();
+                if (keyCode == ' ')
+                {
+                    _pausedUntilSpaceIsPressed = false;
+                }
+                
+            }
+        }
+        else 
+        {
+
+            cout << "\033[2J\033[1;1H";
+        
+            if (stdinHasData()) 
+            {            
+                keyCode = std::cin.get();
+                keyPressed(keyCode);                
+            }
+            
+            // reset translation
+            _xo = 0;
+            _yo = 0;
+            draw();
+            render();
+            if (_description.size())
+            {
+                cout << "SCREEN READER: " << _description << endl;
+            }            
+            console();
+            cout.flush();
+
+        }
+                
+        this_thread::sleep_for(chrono::milliseconds(100));
+    }
+}
+
+
+
+//  returns a random number from 0 up to (but not including) the number.
+int random(int n)
+{    
+    return (rand() % n);
+}
+
+// returns a random number from the first argument up to 
+// (but not including) the second argument.
+int random(int a, int b)
+{
+    return (rand() % (b - a)) + a;
+}
+
+string random(vector<string> choices)
+{
+    int i = random(choices.size());
+    return choices[i];
+}
---
+++ examples.cpp
@@ -0,0 +1,688 @@
+#include "engine.h"
+#include <vector>
+
+
+class ShootGame : public Engine
+{    
+    int x = 0;
+    int y = 0;
+    int dx = 1;
+    int dy = 0;
+    
+    void setup()
+    {
+        createCanvas(30,10); 
+    }
+
+    void keyPressed(int keyCode)
+    {
+        switch (keyCode)
+        {
+            case 'd':        
+                dx = 1;
+                dy = 0;
+                break;
+
+            case 'a':
+                dx = -1;
+                dy = 0;
+                break;
+
+            case 'w':
+                dx = 0;
+                dy = -1;
+                break;
+
+            case 's':
+                dx = 0;
+                dy = 1;
+                break;
+        }
+    }
+
+    void draw()
+    {
+        clear();
+        stroke("$$");
+        point(29, 5);
+
+        stroke("->");
+        point(x, y);
+    }
+
+    void console()
+    {
+        cout << "Try to control the arraw to shoot the target" << endl;          
+        if (x == 29 && y == 5)
+        {
+            cout << "You hit the target!" << endl;            
+            quit();
+        }
+
+        x = x + dx;
+        y = y + dy;
+    }
+
+};
+
+
+class ExploreGame : public Engine
+{
+    int x;    
+    int y;
+    
+    void setup()
+    {
+        x = 0;     
+        y = 0;
+        createCanvas(10, 10);
+    }
+
+    void draw()
+    {
+        clear();
+        stroke("_?");
+        point(5, 5);       
+
+        stroke("_?");
+        point(3, 6);
+
+        stroke("👮");
+        point(x, y); 
+
+        describe("A police man. Two question marks.");
+    }
+
+    void console()
+    {
+        if (x == 5 && y == 5)
+        {
+            cout << "You found a criminal." << endl;
+            x = 6;             
+            pause();
+        }
+        else if (x == 3 && y == 6)
+        {
+            cout << "You found a lost child." << endl;
+            x = 4;
+            pause();
+        }
+    }
+
+    void keyPressed(int keyCode)
+    {
+        switch (keyCode)
+        {
+            case 'd':        
+                x = x + 1;
+                break;
+
+            case 'a':
+                x = x - 1;
+                break;
+
+            case 'w':
+                y = y - 1;
+                break;
+
+            case 's':
+                y = y + 1;
+                break;
+        }
+    }
+};
+
+class PaintGame : public Engine
+{    
+    int x = 0;
+    int y = 0;
+    
+    void setup()
+    {
+        createCanvas(10,10); 
+    }
+
+    void keyPressed(int keyCode)
+    {
+        switch (keyCode)
+        {
+            case 'd':        
+                x = x + 1;
+                break;
+
+            case 'a':
+                x = x - 1;
+                break;
+
+            case 'w':
+                y = y - 1;
+                break;
+
+            case 's':
+                y = y + 1;
+                break;
+        }
+    }
+
+    void draw()
+    {                
+        point(x, y);
+    }
+
+    void console()
+    {
+        cout << "(" << x << "," << y << ")" << endl;
+    }
+
+};
+
+
+
+
+
+class HelloWorld : public Engine
+{
+    void setup()
+    {
+        createCanvas(10,2);
+    }
+
+    void draw()
+    {
+        text("Hello World", 0, 0);
+    }
+};
+
+class PointExample : public Engine
+{
+    void setup()
+    {
+        createCanvas(10,10);
+    }
+
+    void draw()
+    {
+        point(1, 1);
+        point(3, 3);
+        point(5, 5);
+        point(7, 7);        
+
+        describe("Four points.");
+    }
+};
+
+class LineExample : public Engine
+{
+    void setup()
+    {
+        createCanvas(10,10);
+    }
+
+    void draw()
+    {
+        vertical_line(1, 2, 5);
+        vertical_line(3, 2, 5);
+        vertical_line(5, 2, 5);
+
+        horizontal_line(3, 0, 5);
+        horizontal_line(5, 0, 5);
+        horizontal_line(7, 0, 5);
+
+        describe("Three vertical lines and three horizontal lines.");
+    }
+};
+
+class RectExample1 : public Engine
+{
+    void setup()
+    {
+        createCanvas(20,10);
+    }
+
+    void draw()
+    {
+        rect(0, 0, 4, 4);
+
+        rect(7, 3, 4, 4);
+        
+        rect(15, 2, 5, 5);
+
+        describe("Three rectangules.");
+    }
+};
+
+class RectExample2 : public Engine
+{
+    void setup()
+    {
+        createCanvas(20,10);
+    }
+
+    void draw()
+    {
+        fill("**");
+        rect(0, 0, 4, 4);
+
+        rect(7, 3, 4, 4);
+        
+        rect(15, 2, 5, 5);
+
+        describe("Three rectangules filled with *.");
+    }
+};
+
+class RectExample3 : public Engine
+{
+    void setup()
+    {
+        createCanvas(20,10);
+    }
+
+    void draw()
+    {
+        stroke("🌻");
+        fill("🌲");
+        rect(0, 0, 6, 8);
+
+        stroke("🌹");
+        rect(7, 3, 7, 4);
+
+        noFill();
+        rect(15, 2, 5, 5);
+
+        describe("Two rectangular areas of trees surrounded by sunflowers. One rectangular empty area surrounded by flowers.");
+    }
+};
+
+class MoveExample1 : public Engine
+{
+    int x;
+
+    void setup()
+    {
+        x = 0;        
+        createCanvas(30, 10);
+    }
+
+    void draw()
+    {
+        clear();
+        point(x, 0);
+        x = x + 1;        
+
+        describe("A point moves horizontally to the right.");
+    }
+};
+
+class MoveExample2 : public Engine
+{
+    int x1;
+    int x2;
+
+    void setup()
+    {
+        x1 = 0;
+        x2 = 0;
+        createCanvas(30, 5);        
+    }
+
+    void draw()
+    {
+        clear();
+        point(x1, 0);
+        x1 += 1;
+        point(x2, 2);     
+        x2 += 2;
+
+        describe("Two points move horizontally at different speeds.");
+    }
+};
+
+class KeyPressedExample1 : public Engine
+{
+    int x;    
+    int y;
+    void setup()
+    {
+        x = 0;     
+        y = 0; 
+        createCanvas(10, 10);
+    }
+
+    void draw()
+    {
+        clear();
+        point(x, y);   
+
+        describe(string("A point is at (") + to_string(x) + "," + to_string(y) + ")");
+    }
+
+    void keyPressed(int keyCode)
+    {
+        switch (keyCode)
+        {
+            case 'd':        
+                x = x + 1;
+                break;
+
+            case 'a':
+                x = x - 1;
+                break;
+
+            case 'w':
+                y = y - 1;
+                break;
+
+            case 's':
+                y = y + 1;
+                break;
+        }
+    }
+};
+
+class KeyPressedExample2 : public Engine
+{
+    void setup()
+    {
+        createCanvas(10, 10);
+    }
+
+    void draw()
+    {
+        clear();
+        text("Press (Q) to Quit.",0,0);
+    }
+
+    void keyPressed(int keyCode)
+    {
+        switch (keyCode)
+        {
+            case 'q':
+            case 'Q':        
+                quit();
+                break;
+        }
+    }
+};
+
+class StrokeExample : public Engine
+{
+    void setup()
+    {
+        createCanvas(10, 10);
+    }
+
+    void draw()
+    {
+        clear();
+
+        stroke("->");
+        point(1, 1);         
+        horizontal_line(5, 2, 8);
+
+        stroke("🌲");
+        horizontal_line(6, 2, 8);
+    }
+};
+
+class ConsoleInputExample : public Engine
+{
+    int numTrees = 0;
+    int numFlowers = 0;
+
+    void setup()
+    {
+        createCanvas(10, 10);
+    }
+
+    void draw()
+    {
+        clear();
+        stroke("🌲");
+        horizontal_line(2, 0, numTrees);
+
+        stroke("🌻");
+        horizontal_line(4, 0, numFlowers);
+
+        describe("A line of trees and a line of sun flowers");
+    }
+
+    void console()
+    {
+        string s;
+        cout << "How many trees? ";
+        cin >> s;
+        numTrees = stoi(s);        
+        cout << "How many flowers? ";
+        cin >> s;        
+        numFlowers = stoi(s);
+    }
+};
+
+class ResizeCanvasExample : public Engine
+{
+    int size;        
+    void setup()
+    {
+        createCanvas(10, 10);
+    }
+
+    void draw()
+    {
+        clear();
+        
+    }
+
+    void keyPressed(int keyCode)
+    {
+        switch (keyCode)
+        {
+            case '1':        
+                resizeCanvas(10, 10);        
+                break;
+
+            case '2':
+                resizeCanvas(15, 15);        
+                break;
+
+            case '3':
+                resizeCanvas(20, 20);
+                break;
+        }
+    }
+};
+
+
+
+class RandomExample1 : public Engine
+{
+    void setup()
+    {
+        createCanvas(30, 5);
+    }
+
+    void draw()
+    {
+        clear();
+        for (int i = 0; i < 5; i++) {
+            int r = random(15);            
+            horizontal_line(i, 0, r);
+        }
+        describe("Five lines with constantly changing lengths, aligned to the left.");
+    }
+};
+
+class RandomExample2 : public Engine
+{
+    vector<string> choices{"rock", "paper", "scissor"};
+
+    void setup()
+    {
+        createCanvas(30, 5);
+    }
+
+    void draw()
+    {
+        clear();        
+        string c = random(choices);
+        text(c, 0, 0);
+        describe(c);
+    }
+};
+
+class RandomExample3 : public Engine
+{
+    void setup()
+    {
+        createCanvas(30, 5);
+    }
+
+    void draw()
+    {
+        clear();
+        for (int i = 0; i < 5; i++) {
+            int r = random(-5, 5);
+            horizontal_line(i, 10 + r, 10 + r + 10);
+        }
+        describe("Five lines with constantly changing lengths.");
+    }
+};
+
+class BackgroundExample : public Engine
+{
+    void draw()
+    {
+        background("xx");        
+        point(3, 3);
+        point(5, 5);
+        describe("Two points against a background filled by x's.");
+    }
+};
+
+class TranslateExample1 : public Engine
+{
+    void setup()
+    {
+        createCanvas(15, 15);
+    }
+
+    void draw_box()
+    {
+        rect(0, 0, 3, 3);
+        text("Hi", 1, 1);
+    }
+
+    void draw()
+    {
+        draw_box(); // Draw a box at original 0,0
+        
+        translate(5, 5);
+        draw_box(); // Draw a box at new 0,0
+        
+        // Translations are cumulative. For example,
+        // calling translate(2,2)  and translate(3,3)
+        // is equivalent to calling translate(5,5)        
+        translate(2, 2);
+        translate(3, 3);
+        draw_box(); // Draw a box at new 0,0        
+    }
+};
+
+class TranslateExample2 : public Engine
+{
+    int x = 0;
+
+    void setup()
+    {
+        createCanvas(15, 15);
+    }
+
+    void draw_box()
+    {
+        rect(0, 0, 3, 3);
+        text("Hi", 1, 1);
+    }
+
+    void draw()
+    {        
+        // the transformation is reset when the loop begins again.
+
+        clear();
+        translate(x, 0);
+        draw_box();
+        x = x + 1;
+
+        describe("A box is moving horizontally.");
+    }
+};
+
+class MenuExample : public Engine
+{
+    vector<Engine*> games;
+    vector<string> game_names;
+
+    public:
+
+    void add(Engine* game, string name)
+    {
+        games.push_back(game);
+        game_names.push_back(name);
+    }
+
+    void setup()
+    {
+        for (int i = 0 ; i < games.size() ; i++)
+        {
+            cout << (i+1) << ". " << game_names[i] << endl;
+        }
+
+        string c;
+        cout << "Choose a game:";
+        cin >> c;
+        int i = stoi(c);
+        if (i >= 1 && i <= games.size())
+        {
+            games.at(i-1)->play();
+        }
+        else
+        {
+            cout << "Not a valid choice." << endl;
+        }
+        quit();
+    }
+
+};
+
+int main()
+{
+    MenuExample game;        
+    
+    // Games
+    game.add(new PaintGame(), "PaintGame");
+    game.add(new ExploreGame(), "ExploreGame");
+    game.add(new ShootGame(), "ShootGame");
+
+    // Basic Examples
+    game.add(new HelloWorld(), "HelloWorld");
+    game.add(new LineExample(), "LineExample");
+    game.add(new PointExample(), "PointExample");
+    game.add(new StrokeExample(), "StrokeExample");
+    game.add(new RectExample1(), "RectExample1");
+    game.add(new RectExample2(), "RectExample2");
+    game.add(new RectExample3(), "RectExample3");    
+    game.add(new MoveExample1(), "MoveExample1");
+    game.add(new MoveExample2(), "MoveExample2");        
+    game.add(new KeyPressedExample1(), "KeyPressedExample1");
+    game.add(new KeyPressedExample2(), "KeyPressedExample2");
+    game.add(new StrokeExample(), "StrokeExample");
+    game.add(new RandomExample1(), "RandomExample1");
+    game.add(new RandomExample2(), "RandomExample2");
+    game.add(new RandomExample3(), "RandomExample3");    
+    game.add(new ResizeCanvasExample(), "ResizeCanvasExample"); 
+    game.add(new BackgroundExample(), "BackgroundExample");
+    game.add(new TranslateExample1(), "TranslateExample1");
+    game.add(new TranslateExample2(), "TranslateExample2");
+    game.play();
+
+    return 0;
+}
